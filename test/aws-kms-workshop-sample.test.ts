35bccf61626ffd112b1e331e7de681cd1ec1563e
seyong <sykang@amazon.com>
2024-01-11 19:42:20 +0900

    Initial commit

---
+++ test/aws-kms-workshop-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsKmsWorkshopSample from '../lib/aws-kms-workshop-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-kms-workshop-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsKmsWorkshopSample.AwsKmsWorkshopSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
